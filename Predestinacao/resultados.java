daa8b7f7305d1722956d5db2a1782e627f711844
EdilsonRamosJunior <edilson.ramosjr123@gmail.com>
2021-10-02 17:48:35 -0300

    d

---
+++ Predestinacao/resultados.java
@@ -0,0 +1,129 @@
+public class resultados {
+	
+ public static String resultado1() {
+  return "Após alguns meses no seu novo emprego.\n"
+    + "Você conhece um homem misterioso, porém\n"
+    + "interessante. Ele desperta algo diferente\n"
+    + "você. Vocês passam uma noite juntos. No\n"
+    + "dia seguinte, ele some da sua vida.";
+ }
+ public static String resultado2() {
+  return "Após algumas semanas, você percebe\n"
+    + "que está grávida.";
+ }
+ public static String resultado3(){
+  return "No parto, o médico relata que houve complicações.\n"
+    + "e tiveram que fazer uma histerectomia. Ele \n"
+    + "percebeu que você possui o orgão masculino pouco\n"
+    + "desenvolvido. Devido a isso, fizeram algumas mudanças\n"
+    + "em seu corpo durante a cirugia. Então você passaria\n"
+    + "por um tratamento hormonal para mudança de sexo.";
+}
+ public static String resultado4() {
+  return "Procurou por meses e não encontra seu filho.\n"
+    + "-Vale a pena viver?\n"
+    + "[1]sim [2]não";
+ }
+ public static String resultado5() {
+  return "Você desistiu.";
+ }
+ public static String resultado6() {
+  return "Você decide viver sua nova vida como homem.\n"
+    + "Certo dia, encontra um homem em\n"
+    + "um bar e conta a sua história para\n"
+    + "ele. Ele pergunta o que você faria\n"
+    + "se ficasse de frente para o homem \n"
+    + "que largou você grávida. Você responde:\n"
+    + "- Eu mataria ele.";
+ }
+ public static String resultado7() {
+  return "Entao você rouba a maquina temporal e vai para o futuro.\n"
+    + "Infelizmente a data da máquina estava programada\n"
+    + "para o exato momento de um ataque terrorista em Nova York\n"
+    + "que matou cinco mil pessoas.\n"
+    + "Você e Lucy morrem no atentado.";
+ }
+ public static String resultado8() {
+  return "Você quebra os protocolos da agência.\n"
+    + "Alguns agentes te acham e você é morto";
+  
+ }
+ public static String resultado9() {
+  return "Você viaja no tempo e descobre que o traidor\n"
+    + "é você mesmo de outra linha temporal.\n"
+    + "Ao matá-lo, você enlouquece e começa a\n"
+    + "realizar diversos atendado terroristas pelo planeta.";
+ }public static String resultado10() {
+  return "Você deve viajar para um bar no passado\n"
+    + "recrutar um homem que se chamava Lucy.\n"
+    + "Você faz a mesma proposta de colocá-lo\n"
+    + "de frente para o responsável dos seus\n"
+    + "problemas. Assim que você o deixa no passado,\n"
+    + "volta novamente no tempo para roubar o filho\n"
+    + "da Lucy e deixar o bebê no orfanato Berkeley.";
+ }
+	
+	
+ public static String resultado11() {
+  return "Você encontra o terrorista e descobre que ele\n"
+    + "é você do futuro.\n"
+    + "Você consegue matá-lo, contudo você começa a ter\n"
+    + "sinais de loucura.";
+ }
+ public static String resultado111() {
+  return "O seu eu do passado encontra você e te mata.";
+ }
+	
+ public static String resultado12() {
+   return "Você encontra uma mulher para casar.\n"
+     + "Mas o excessivo número de viagem temporais\n"
+     + "faz com que você desenvolva demência.";
+}
+ public static String caminho1_1() {
+  return "[1]Estudar muito para se destacar dos outros";
+ }
+ public static String caminho1_2() {
+  return "Ao decidir estudar para ser diferente\n"
+    + "Você se destaca na escola.\n"
+    + "Certo dia, surge uma oportunidade de\n"
+    + "mudar de vida.\n"
+    + " \n"
+    + "[1]Entrar para um programa espacial.\n"
+    + "[2]Aceitar entrar em uma agência do governo.";
+ }
+ public static String caminho1_3(){
+  return "Ao contar a verdade, Lucy não acredita. Você\n"
+    + "enlouquece e volta com o cara do bar ao futuro.\n"
+    + "Em seguida, rouba a máquina do homem.";
+ }
+ public static String caminho1_4(){
+  return "Ao voltar para o futuro, o cara do bar(James)\n"
+    + "convida você para fazer parte da mesma agência\n"
+    + "temporal que ele trabalha. Ao se tornar uma agente.\n"
+    + "O que vc deseja fazer?";
+ }
+ public static String caminho1_5(){
+  return "Ao tentar pegar um terrorista no futuro,\n"
+    + "você quase foi morto por ele. Seu rosto\n"
+    + "foi todo queimado na missão.";
+ }
+ public static String caminho2_1() {
+  return "[2]Ficar rebelde sem se importar consigo mesma.";
+ }
+ public static String caminho2_2() {
+  return "A vida foi muito dura para você, então\n"
+    + "decide concentrar toda\n"
+    +"sua raiva em algum lugar. Com isso,\n"
+    + "se alista no exército.";
+ }
+ public static String caminho3() {
+  return "Vocês se apaixonam instantaneamente e \n"
+    + "passam a noite juntos. Nesse momento,\n"
+    + "você percebe que é o homem que estragou\n"
+    + "sua vida no passado.\n"
+    + "O cara do bar começa a chamar você para voltarem\n"
+    + "ao futuro.\n"
+    + "[1]Fica com Lucy para viverem juntos?\n"
+    + "[2]Você volta com ele?";
+ }
+}
